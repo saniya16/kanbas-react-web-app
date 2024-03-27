bd3dca4868c0df0216a258644a72be932f9520ee
saniya <saniyaanklesaria16@gmail.com>
2024-03-27 19:35:22 -0400

    Lab 5 started

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -4,6 +4,7 @@ import Assignment3 from "./a3";
 import Assignment4 from "./a4";
 import store from "./store";
 import { Provider } from "react-redux";
+import Assignment5 from "./a5";
 
 function Labs() {
       return (
@@ -12,10 +13,12 @@ function Labs() {
           <h1>Labs</h1>
           <Nav />
           <Link to="/Labs/a3">Assignment 3</Link> |
-          <Link to="/Labs/a4">Assignment 4</Link>
+          <Link to="/Labs/a4">Assignment 4</Link> | 
+          <Link to="/Labs/a5">Assignment 5</Link>
           <Routes>
             <Route path="/a3/*" element={< Assignment3 />} />
             <Route path="/a4" element={< Assignment4 />} />
+            <Route path="/a5" element={< Assignment5 />} />
           </Routes>
         </div>
         </Provider>
